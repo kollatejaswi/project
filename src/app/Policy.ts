d54fdacd409f80356afbb1ff07b8b2f90f8e6c29
kollatejaswi <saitejaswi0123@gmail.com>
2021-02-18 11:00:13 +0530

    first commit

---
+++ src/app/Policy.ts
@@ -0,0 +1,8 @@
+export class Policy{
+    id:number;
+    name: string;
+    type: string;
+    duration:number;
+    amount: number;
+
+}
---
+++ src/app/registerpolicy/registerpolicy.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminserviceService } from '../adminservice.service';
+import { Policy } from '../Policy';
+
+@Component({
+  selector: 'app-registerpolicy',
+  templateUrl: './registerpolicy.component.html',
+  styleUrls: ['./registerpolicy.component.css']
+})
+export class RegisterpolicyComponent implements OnInit {
+  policy : Policy=new Policy();
+  duedate:string
+  constructor(private customerService:AdminserviceService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.editPolicy();
+    this.duedate='01/03/2020'
+  }
+  editPolicy()
+  {
+    let id=localStorage.getItem("id");
+    this.customerService.getPolicy(+id)
+        .subscribe(data=>{
+          this.policy=data;
+        })
+  }
+  onUpdate(){    
+    this.router.navigate(["success"])
+    
+  }
+}
